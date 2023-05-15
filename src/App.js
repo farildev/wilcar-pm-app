bd52c456d1b2d127cd2e7abb1858aa57c522aaf4
farildev <memmedovfaril@gmail.com>
2023-05-15 21:02:08 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import {Routes, Route} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import AddProducts from "./pages/AddProducts";
+import "./app.css";
 function App() {
   return (
-    <div className="App">
-      
+    <div className="wrapper-container">
+      <Sidebar/>
+      <div className="wrapper">
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/products" element={<Products/>} />
+        <Route path="/products/add" exact={true} element={<AddProducts/>} />
+      </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import "../assets/css/sidebar.css";
+import logo from "../assets/img/Wrıst media logo 1.png";
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="logo">
+        <img src={logo} alt="Wrist Media Logo" />
+      </div>
+      <div className="side__nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink to="/">Dashboard</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/products">Products</NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink to="/products/add">Add Products</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
