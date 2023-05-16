fc75a57f33636466bda40d64338869784cdbb0c7
farildev <memmedovfaril@gmail.com>
2023-05-16 23:35:16 +0400

    UPDATE

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react';
 
 function Products() {
+  const [products , setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  },[])
   return (
-    <div>Products</div>
+    <div className='products container'>
+      
+   </div>
   )
 }
 
