9de456c029eb36afd0532bca961b57178b2153b6
farildev <memmedovfaril@gmail.com>
2023-05-17 15:32:07 +0400

    UPDATE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,18 @@ import {Link} from 'react-router-dom';
 
 function Card({detail}) {
   return (
-    <div>
-        <div className="card" style="width: 18rem;">
-        <img src={detail.image} className="card-img-top" alt={detail.title} />
+    <>
+      <div className='row'>
+        <div className="card">
+        <img src={detail?.image} className="card-img-top" alt={detail.title} />
         <div className="card-body">
-            <h5 className="card-title">{detail.title}</h5>
-            <p className="card-text">{detail.description}</p>
+            <h5 className="card-title">{detail?.title}</h5>
+            <p className="card-text">{detail?.description}</p>
         </div>
+        <Link className=''></Link>
         </div>
     </div>
+    </>
   )
 }
 
