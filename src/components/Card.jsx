c42a819d8ac125eec0775c54ac212ab40bd764f9
farildev <memmedovfaril@gmail.com>
2023-05-19 22:43:18 +0400

    UPDATE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-
+import "../assets/css/card.css";
 function Card({detail}) {
   return (
     <>
       <div className='row'>
-        <div className="card">
+        <div className="card col-4">
         <img src={detail?.image} className="card-img-top" alt={detail.title} />
         <div className="card-body">
             <h5 className="card-title">{detail?.title}</h5>
             <p className="card-text">{detail?.description}</p>
         </div>
-        <Link className=''></Link>
+        <Link className='btn btn-primary'>View</Link>
         </div>
     </div>
     </>
