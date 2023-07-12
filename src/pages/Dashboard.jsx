a015ca2b2ed4412dc790a42d5061caa39a54a32e
farildev <memmedovfaril@gmail.com>
2023-07-12 13:16:18 +0400

    UPDATE

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,19 +3,19 @@ import "../assets/css/dashboard.css";
 
 function Dashboard() {
   return (
-    <>
+    <div className='dashboard'>
       <div className="row p-2">
         <div className="header">
           <h1>Add Products</h1>
         </div>
-        <div className="form__control col-4 mt-2">
+        <div className="form__control col-6 mt-2">
           <input type="text" className="p-2"  placeholder="Enter product name"/>
-          <input type="text"  placeholder="Enter product name"/>
-          <input type="text"  placeholder="Enter product name"/>
-          <input type="text"  placeholder="Enter product name"/>
+          <input type="text" className="p-2"  placeholder="Enter product price"/>
+          <input type="text" className="p-2"  placeholder="Enter product description"/>
+          <input type="text" className="p-2"  placeholder="Enter product id"/>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
