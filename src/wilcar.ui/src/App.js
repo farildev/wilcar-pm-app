838e92537d50fff159b6ae7acd279afb40c667ba
Azer Suleymanli <suleymanliazer9@gmail.com>
2023-07-28 21:55:05 +0400

    Refactor routing with lazy loading

--- src/wilcar.ui/src/App.js
+++ src/wilcar.ui/src/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products";
-import Home from "./pages/Home";
-import Edit from "./pages/Edit";
 import "./app.css";
 
 
+const Home = React.lazy(() => import("./pages/Home"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Products = React.lazy(() => import("./pages/Products"));
+const Edit = React.lazy(() => import("./pages/Edit"));
+
 function App() {
   return (
-    <>
-      <div className="wrapper-container">
-        <Sidebar />
-        <div className="wrapper">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:id/edit" element={<Edit />} />
-          </Routes>
-        </div>
+    <div className="wrapper-container">
+      <Sidebar />
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<React.Suspense fallback={<div>Loading...</div>}><Home /></React.Suspense>} />
+          <Route path="/dashboard" element={<React.Suspense fallback={<div>Loading...</div>}><Dashboard /></React.Suspense>} />
+          <Route path="/products" element={<React.Suspense fallback={<div>Loading...</div>}><Products /></React.Suspense>} />
+          <Route path="/products/:id/edit" element={<React.Suspense fallback={<div>Loading...</div>}><Edit /></React.Suspense>} />
+        </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
